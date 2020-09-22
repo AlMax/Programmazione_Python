0d479579d89bc705aaceb58c41213f51907a389c
Ali Haider Maqsood <alihaider.maqsood@gmail.com>
2020-09-22 23:58:40 +0200

    codice migliorato
    l'obbiettivo è creare una funzione che gestisca tutta questa implementazione

--- Buttons.py
+++ Buttons.py
@@ -1,50 +1,61 @@
-# importing tkinter and tkinter.ttk
-# and all their functions and classes
 from tkinter import *
 from tkinter.ttk import *
 import sys
 import os
-
-# importing askopenfile function
-# from class filedialog
 from tkinter.filedialog import askopenfilename
 
-root = Tk()
-root.title("Programmino")
-root.geometry('320x180')
-root.resizable(0, 0)
-label = Label(root, text="Selezionare il PDF da leggere\n\n\nSelezionare l'Excel da leggere\n\n\nInserire il nome della cella").pack(side= LEFT,anchor = NW, pady = 12, padx = 10)
+def caricaFile(button, fileType):
+    file = askopenfilename(filetypes = fileType)
 
+    if file != "":
+        button['text'] = os.path.basename(file)
+        button['state'] = "disabled"
+        return
+    button['text'] = "Errore! Riprovare"
+    print(file + "\t" + button['text'])
 
-valore = StringVar()
-nome=""
-bottoni = []
-# This function will be used to open
-# file in read mode and only Python files
-# will be opened
-def open_file(i):
-    file = askopenfilename(filetypes = [('FILE PDF', '*.pdf')])
-    nome = file
-    btn = bottoni[i]
-    btn['text'] = os.path.basename(nome)
-    btn['state'] = "disabled"
-    print(file)
-
-def submit():
+def modifica(all_buttons, all_texts):
     print(valore.get())
-    btn = bottoni[0]
-    btn['text'] = 'Seleziona file PDF'
-    btn['state'] = "enabled"
+    indice = 0
+    for button in all_buttons:
+        button['text'] = all_texts[indice]
+        button['state'] = "enabled"
+        indice += 1
 
-bottoni.append(Button(root, text ='Seleziona file PDF', command = lambda:open_file(0)))
-bottoni.append(Button(root, text ='Seleziona file Excel', command = lambda:open_file(1)))
-bottoni.append(Button(root, text ='MODIFICA', command = lambda:submit()))
-bottoni.append(Button(root, text ='CONFERMA', command = lambda:sys.exit()))
+try:
+    nome_programma = "Programmino"
+    buttons = []
+    texts = []
 
-bottoni[0].pack(side = TOP, anchor = NW, pady = 10)
-bottoni[1].pack(side = TOP, anchor = NW, pady = 10)
-bottoni[2].place(relx=0.3, rely=0.83, anchor=CENTER)
-bottoni[3].place(relx=0.7, rely=0.83, anchor=CENTER)
-testo = Entry(root, textvariable=valore, width = 20).pack(anchor = NW, pady = 10)
+    txt_pdf = "Selezione il file PDF"
+    txt_xls = "Selezione il file EXCEL"
+    txt_label = "Selezionare il PDF da leggere\n\n\nSelezionare l'Excel da leggere\n\n\nInserire il nome della cella"
+    texts.extend([txt_pdf, txt_xls, txt_label])
 
-root.mainloop()
+    root = Tk()
+    root.title(nome_programma)
+    root.geometry('385x180')
+    root.resizable(0, 0)
+
+    valore = StringVar()
+
+    label = Label(root, text= txt_label).pack(side= LEFT,anchor = NW, pady = 12, padx = 10)
+
+    btn_pdf = Button(root, text = txt_pdf, command = lambda:caricaFile(btn_pdf, [(txt_pdf, "*.pdf")]))
+    btn_xls = Button(root, text = txt_xls, command = lambda:caricaFile(btn_xls, [(txt_xls, "*.xls"), (txt_xls, "*.xlsx")]))
+    field_txt = Entry(root, textvariable=valore, width = 15)
+    buttons.extend([btn_pdf, btn_xls])
+
+    btn_modifica = Button(root, text ='MODIFICA', command = lambda:modifica(buttons, texts))
+    btn_conferma = Button(root, text ='CONFERMA', command = lambda:sys.exit(0))
+
+    btn_pdf.pack(side = TOP, anchor = NW, pady = 10)
+    btn_xls.pack(side = TOP, anchor = NW, pady = 10)
+    field_txt.pack(anchor = NW, pady = 10)
+    btn_modifica.place(relx=0.3, rely=0.83, anchor=CENTER)
+    btn_conferma.place(relx=0.7, rely=0.83, anchor=CENTER)
+   
+
+    root.mainloop()
+except Exception as errore:
+    print(str(errore))
