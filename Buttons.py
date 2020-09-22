6e01788f829cc630d1a058f66be77fc8d532f5b7
AlMax <alihaider.maqsood@gmail.com>
2020-09-22 15:58:24 +0200

    creata form strafiga

--- Buttons.py
+++ Buttons.py
@@ -10,34 +10,42 @@ import os
 from tkinter.filedialog import askopenfilename
 
 root = Tk()
-root.geometry('300x200')
+root.title("Programmino")
+root.geometry('320x180')
+root.resizable(0, 0)
+label = Label(root, text="Selezionare il PDF da leggere\n\n\nSelezionare l'Excel da leggere\n\n\nInserire il nome della cella").pack(side= LEFT,anchor = NW, pady = 12, padx = 10)
 
+
+valore = StringVar()
 nome=""
 bottoni = []
 # This function will be used to open
 # file in read mode and only Python files
 # will be opened
-def open_file():
+def open_file(i):
     file = askopenfilename(filetypes = [('FILE PDF', '*.pdf')])
     nome = file
+    btn = bottoni[i]
     btn['text'] = os.path.basename(nome)
     btn['state'] = "disabled"
     print(file)
 
-btn = Button(root, text ='Nome PDF', command = lambda:open_file())
-btn2 = Button(root, text ='Nome Excel', command = lambda:open_file())
-btn3 = Button(root, text ='Nome cella', command = lambda:open_file())
-btn4 = Button(root, text ='Esci', command = lambda:sys.exit())
-btn.pack(side = TOP, pady = 10)
-btn2.pack(side = TOP, pady = 10)
-btn3.pack(side = TOP, pady = 10)
-btn4.pack(side = TOP, pady = 10)
+def submit():
+    print(valore.get())
+    btn = bottoni[0]
+    btn['text'] = 'Seleziona file PDF'
+    btn['state'] = "enabled"
 
-bottoni.append(btn)
-bottoni.append(btn2)
-bottoni.append(btn3)
-bottoni.append(btn4)
+bottoni.append(Button(root, text ='Seleziona file PDF', command = lambda:open_file(0)))
+bottoni.append(Button(root, text ='Seleziona file Excel', command = lambda:open_file(1)))
+bottoni.append(Button(root, text ='MODIFICA', command = submit))
+bottoni.append(Button(root, text ='CONFERMA', command = lambda:sys.exit()))
+bottoni[0].pack(side = TOP, anchor = NW, pady = 10)
+bottoni[1].pack(side = TOP, anchor = NW, pady = 10)
 
-for i in bottoni:
-    print("ojm")
-    mainloop()
+
+bottoni[2].place(relx=0.3, rely=0.83, anchor=CENTER)
+bottoni[3].place(relx=0.7, rely=0.83, anchor=CENTER)
+testo = Entry(root, textvariable=valore, width = 20).pack(anchor = NW, pady = 10)
+
+root.mainloop()
